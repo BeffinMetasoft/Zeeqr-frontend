20c2cd1988237960c7f5af86863434ec1d180b11
BeffinMetasoft <beffin@metasoftit.com>
2023-03-02 15:47:33 +0530

    first commit

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar  from '../components/navbar/Navbar'
+
+function signupPage() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default signupPage
